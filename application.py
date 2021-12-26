cf5e7ffee3154fd60a65867fdc05e085fe5e6d4e
Thiago Lima <thenriq@gmail.com>
2021-12-26 22:06:02 +0000

    included new FLASK functions and urls

--- application.py
+++ application.py
@@ -18,5 +18,25 @@ def start():
 def getBook(id):
     return "you want book with " + str(id)
 
+@app.route('/user')
+def getUser():
+    return "served by getUser"
+
+@app.route('/user/<int:id>')
+def findByIdUser(id):
+    return "served by findByUser with id = "+str(id)
+
+@app.route("/demo_url_for/")
+def demoUrlFor():
+    returnString = "url For index is " + url_for('index')
+    returnString += "<br/>"
+    returnString += "url for findByIdUser " + url_for('findByIdUser', id=12)
+    return returnString
+
+@app.route("/demo_request", methods=['POST', 'GET', 'DELETE'])
+def demoRequest():
+    return request.method
+
+
 if __name__ == '__main__' :
     app.run(debug = True)
