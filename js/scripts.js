e5ffbceace913fe7a470ac39c6c5ca7579b0f79d
Thiago Lima <thenriq@gmail.com>
2021-12-27 23:34:45 +0000

    scketched ajax scripts for crude CURL operations

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,64 @@
+function GetMovie(){
+    $.ajax({
+        "url" : "http://127.0.0.1:5000/demo_request",
+        "method": "GET",
+        "data": "",
+        "dataType" : "HTML",
+        "success" : function (result) {
+            console.log(result);
+        },
+        "error": function (xhr, status, error) {
+            console.log("error: " + status + " msg:" + error); 
+        }
+    });
+}
+
+function NewMovie(){
+    $.ajax({
+        "url" : "http://127.0.0.1:5000/demo_request",
+        "method": "POST",
+        "data": "",
+        "dataType" : "HTML",
+        "success" : function (result) {
+            console.log(result);
+        },
+        "error": function (xhr, status, error) {
+            console.log("error: " + status + " msg:" + error); 
+        }
+    });
+}
+
+function EditMovie(){
+    $.ajax({
+        "url" : "http://127.0.0.1:5000/demo_request",
+        "method": "PUT",
+        "data": "",
+        "dataType" : "HTML",
+        "success" : function (result) {
+            console.log(result);
+        },
+        "error": function (xhr, status, error) {
+            console.log("error: " + status + " msg:" + error); 
+        }
+    });
+}
+
+function DeleteMovie(){
+    $.ajax({
+        "url" : "http://127.0.0.1:5000/demo_request",
+        "method": "DELETE",
+        "data": "",
+        "dataType" : "HTML",
+        "success" : function (result) {
+            console.log(result);
+        },
+        "error": function (xhr, status, error) {
+            console.log("error: " + status + " msg:" + error); 
+        }
+    });
+}
+
+
 $(document).click(function(){
     $("#BtnSearchMovie").click(function(){
         $("#searchform").modal();
