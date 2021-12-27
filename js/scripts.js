d59efbe58e13842a1476a3626ba33d5d1dad5da7
Thiago Lima <thenriq@gmail.com>
2021-12-27 21:56:34 +0000

    separated js code from main html

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).click(function(){
+    $("#BtnSearchMovie").click(function(){
+        $("#searchform").modal();
+    });
+
+});
+
+$(document).click(function(){
+    $("#BtnLogin").click(function(){
+        $("#loginModal").modal();
+    });
+});
