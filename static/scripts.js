6e24a6adb218d74a4913d1094b9e67c64e760e0d
Thiago Lima <thenriq@gmail.com>
2022-01-04 01:19:57 +0000

    renamed scrips folder to static folder - will attempt later to link a js file to pages

---
+++ static/scripts.js
@@ -0,0 +1,91 @@
+host = window.location.origin
+/*
+function login(){
+    $.ajax({
+        "url" : host + "/login",
+        "method" : "POST",
+        "data" : "",
+        "dataType" : "HTML",
+        "success" : function (result) {
+            console.log(result);
+        },
+        "error": function (xhr, status, error) {
+            console.log("error: " + status + " msg:" + error); 
+        }
+    });
+}
+
+*/
+function GetMovie(){
+    $.ajax({
+        "url" : host + "/demo_request",
+        "method": "GET",
+        "data": "",
+        "dataType" : "HTML",
+        "success" : function (result) {
+            console.log(result);
+        },
+        "error": function (xhr, status, error) {
+            console.log("error: " + status + " msg:" + error); 
+        }
+    });
+}
+
+function NewMovie(){
+    $.ajax({
+        "url" : host + "/demo_request",
+        "method": "POST",
+        "data": "",
+        "dataType" : "HTML",
+        "success" : function (result) {
+            console.log(result);
+        },
+        "error": function (xhr, status, error) {
+            console.log("error: " + status + " msg:" + error); 
+        }
+    });
+}
+
+function EditMovie(){
+    $.ajax({
+        "url" : host + "/demo_request",
+        "method": "PUT",
+        "data": "",
+        "dataType" : "HTML",
+        "success" : function (result) {
+            console.log(result);
+        },
+        "error": function (xhr, status, error) {
+            console.log("error: " + status + " msg:" + error); 
+        }
+    });
+}
+
+function DeleteMovie(){
+    $.ajax({
+        "url" : host + "/demo_request",
+        "method": "DELETE",
+        "data": "",
+        "dataType" : "HTML",
+        "success" : function (result) {
+            console.log(result);
+        },
+        "error": function (xhr, status, error) {
+            console.log("error: " + status + " msg:" + error); 
+        }
+    });
+}
+
+
+$(document).click(function(){
+    $("#BtnSearchMovie").click(function(){
+        $("#searchform").modal();
+    });
+
+});
+
+$(document).click(function(){
+    $("#BtnLogin").click(function(){
+        $("#loginModal").modal();
+    });
+});
